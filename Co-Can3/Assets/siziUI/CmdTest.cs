447c7d1edd05909bb1bbc78bb4d641f4985c9888
banri1290 <banri_129@icloud.com>
2025-08-22 19:23:06 +0900

    アニメーションプログラム

--- Co-Can3/Assets/siziUI/CmdTest.cs
+++ Co-Can3/Assets/siziUI/CmdTest.cs
@@ -1,28 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;  // TextMeshPro用
 
 public class CmdTest : MonoBehaviour
 {
-    private int CmdAct = 0;
-    [SerializeField] private Animator characterAnimator; // キャラクターのAnimator
-
-    // ボタンのOnClickイベントに登録
-    public void SetCmdAct()
+    [System.Serializable]
+    public class TextSet
     {
-        CmdAct = 1;
+        public TMP_Text leftText;   // 左のテキスト
+        public TMP_Text rightText;  // 右のテキスト
+        public Animator animator;   // 1セットに1つのAnimator
     }
 
-    void Update()
+    public List<TextSet> sets = new List<TextSet>(); // セットをInspectorに登録
+    private int currentSet = 0;
+
+    // EnterボタンのOnClickイベントに登録
+    public void SetCmdAct()
     {
-        if (CmdAct == 1)
+        if (currentSet < sets.Count)
         {
-            if (characterAnimator != null)
-            {
-                characterAnimator.SetTrigger("PlayAnim"); 
-            }
-            CmdAct = 0; 
+            PlayAnimationsForSet(sets[currentSet]);
+            currentSet++;
+        }
+        else
+        {
+            Debug.Log("すべてのセットのアニメーションが終了しました");
+        }
+    }
+
+    private void PlayAnimationsForSet(TextSet set)
+    {
+        // 左と右のテキストを連結して判定（例: "走る+ジャンプ"）
+        string combo = set.leftText.text + "+" + set.rightText.text;
+        TriggerAnimationByText(combo, set.animator);
+    }
+
+    private void TriggerAnimationByText(string combo, Animator animator)
+    {
+        if (animator == null) return;
+
+        switch (combo)
+        {
+            case "ninzinn+kiru":
+                animator.SetTrigger("B2C");
+                break;
+            case "攻撃+防御":
+                animator.SetTrigger("AttackDefendAnim");
+                break;
+            case "魔法+回復":
+                animator.SetTrigger("MagicHealAnim");
+                break;
+            default:
+                animator.SetTrigger("DefaultAnim");
+                break;
         }
     }
 }
